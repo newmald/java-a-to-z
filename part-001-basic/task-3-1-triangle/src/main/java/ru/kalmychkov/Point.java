94bf6262bb61ff692cf01d50f392653e62ada89d
mald <mald>
2016-10-28 00:02:10 +0400

    add Point.java

---
+++ part-001-basic/task-3-1-triangle/src/main/java/ru/kalmychkov/Point.java
@@ -0,0 +1,15 @@
+package ru.kalmychkov;
+
+public class Point {
+	public double x;
+	public double y;
+	
+	public Point (double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public double distanceTo(Point point) {
+		return Math.sqrt(Math.pow(point.y - this.y, 2) + Math.pow(point.x - this.x, 2));
+	}
+}
