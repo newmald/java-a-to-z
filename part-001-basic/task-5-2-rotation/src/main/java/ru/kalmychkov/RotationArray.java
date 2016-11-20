0acfe3b3456a550901090096f2acfdf4663dcf24
mald <mald>
2016-11-21 00:14:45 +0400

    edit RotationArray.java

--- part-001-basic/task-5-2-rotation/src/main/java/ru/kalmychkov/RotationArray.java
+++ part-001-basic/task-5-2-rotation/src/main/java/ru/kalmychkov/RotationArray.java
@@ -7,11 +7,17 @@ package ru.kalmychkov;
  * @version 1.0
  */
 public class RotationArray {
+	/**
+     * rotation - метод поворота массива.
+     * @param values - массив, который необходимо повернуть
+     * @return - сохраняется массив
+     */
 	public int[][] rotation(int[][] values) {
 		for (int i = 0; i < values.length / 2; i++) {
 			for (int j = i; j < values.length - 1 - i; j++) {
 				int temp = values[i][j];
                 values[i][j] = values[j][values.length - 1 - i];
+                values[j][values.length - 1 - i] = values[values.length - 1 - i][values.length - 1 - j];
                 values[values.length - 1 - i][values.length - 1 - j] = values[values.length - 1 - j][i];
                 values[values.length - 1 - j][i] = temp;
             }
