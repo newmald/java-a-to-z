344806602615f7b711b6db7b0f97a210aba8eb62
mald <mald>
2016-11-20 19:43:53 +0400

    add RotationArray.java

---
+++ part-001-basic/task-5-2-rotation/src/main/java/ru/kalmychkov/RotationArray.java
@@ -0,0 +1,21 @@
+package ru.kalmychkov;
+
+/**
+ * Программа поворота двухмерного квадратного массива на 90 градусов.
+ * @author Алексей Калмычков
+ * @since 20.11.2016
+ * @version 1.0
+ */
+public class RotationArray {
+	public int[][] rotation(int[][] values) {
+		for (int i = 0; i < values.length / 2; i++) {
+			for (int j = i; j < values.length - 1 - i; j++) {
+				int temp = values[i][j];
+                values[i][j] = values[j][values.length - 1 - i];
+                values[values.length - 1 - i][values.length - 1 - j] = values[values.length - 1 - j][i];
+                values[values.length - 1 - j][i] = temp;
+            }
+		}
+        return values;
+	}
+}
