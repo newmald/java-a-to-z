cd547e33bd86b5cf8af14d1c5e601647fb3f28c6
mald <mald>
2016-11-24 01:18:08 +0400

    add RemoveDuplicateTest.java

---
+++ part-001-basic/task-5-3-remove-duplicate/src/test/java/ru/kalmychkov/RemoveDuplicateTest.java
@@ -0,0 +1,26 @@
+package ru.kalmychkov;
+
+import org.junit.Assert;
+import org.junit.Test;
+import java.util.Arrays;
+import static org.hamcrest.CoreMatchers.is;
+/**
+ * Тест удаления дубликатов массива.
+ * @author Алексей Калмычков
+ * @since 23.11.2016
+ * @version 1.0
+ */
+public class RemoveDuplicateTest {
+    /**
+     * whenAddStringArrayThenRemoveDuplicate - метод проверки удаления дубликата.
+     */
+    @Test
+    public void whenAddStringArrayThenRemoveDuplicate() {
+        String[] name = {"Alex", "Alex", "Max", "Peter", "Mike", "John", "Max"};
+        RemoveDuplicate removeDuplicate = new RemoveDuplicate();
+        String[] result = removeDuplicate.removeCopy(name);
+        String[] expected = {"Alex", "Max", "Peter", "Mike", "John"};
+        Assert.assertThat(result, is(expected));
+        System.out.println(Arrays.toString(result));
+    }
+}
