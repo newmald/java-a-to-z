1a799bdc9258da775b5a38aae0cb43c4fbec6966
mald <mald>
2016-12-04 21:18:19 +0400

    edit SortingArraysTest

--- part-001-basic/task-6-2-sorting-arrays/src/test/java/ru/kalmychkov/SortingArraysTest.java
+++ part-001-basic/task-6-2-sorting-arrays/src/test/java/ru/kalmychkov/SortingArraysTest.java
@@ -12,13 +12,39 @@ import static org.hamcrest.CoreMatchers.is;
  */
 public class SortingArraysTest {
     /**
-     * whenAddTwoArraysThenCreateArray - метод проверки создания массива из двух отсортированных массивов.
+     * whenAddTwoArraysThenCreateArray - метод проверки создания массива из двух отсортированных массивов одинаковой длины.
      */
     @Test
     public void whenAddTwoSortedArraysThenCreateArray() {
         final int[] arrayOne = {0, 2, 4, 6, 8};
         final int[] arrayTwo = {1, 3, 5, 7, 9};
         final int[] expected = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        SortingArrays sortingArrays = new SortingArrays();
+        int[] result = sortingArrays.sorting(arrayOne, arrayTwo);
+        Assert.assertThat(result, is(expected));
+    }
+
+    /**
+     * whenAddTwoArraysFirstLongThenCreateArray - метод проверки создания массива из двух отсортированных массивов (первый массив длиннее).
+     */
+    @Test
+    public void whenAddTwoSortedArraysFirstLongThenCreateArray() {
+        final int[] arrayOne = {0, 2, 4, 6, 8, 10};
+        final int[] arrayTwo = {1, 3, 5, 7, 9};
+        final int[] expected = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        SortingArrays sortingArrays = new SortingArrays();
+        int[] result = sortingArrays.sorting(arrayOne, arrayTwo);
+        Assert.assertThat(result, is(expected));
+    }
+
+    /**
+     * whenAddTwoArraysThenCreateArray - метод проверки создания массива из двух отсортированных массивов (второй массив длиннее).
+     */
+    @Test
+    public void whenAddTwoSortedArraysSecondLongThenCreateArray() {
+        final int[] arrayOne = {0, 2, 4, 6, 8};
+        final int[] arrayTwo = {1, 3, 5, 7, 9, 10};
+        final int[] expected = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         SortingArrays sortingArrays = new SortingArrays();
         int[] result = sortingArrays.sorting(arrayOne, arrayTwo);
         Assert.assertThat(result, is(expected));
