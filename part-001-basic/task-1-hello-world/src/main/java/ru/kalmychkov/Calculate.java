c3937f2565c3ced839aa9ce7aeaa38392b95d0fa
mald <mald>
2016-12-04 20:31:20 +0400

    edit Hello

---
+++ part-001-basic/task-1-hello-world/src/main/java/ru/kalmychkov/Calculate.java
@@ -0,0 +1,17 @@
+package ru.kalmychkov;
+
+/**
+ * Вывод Hello, world.
+ * @author Алексей Калмычков
+ * @since 04.12.2016
+ * @version 1.0
+ */
+public class Calculate {
+	/**
+	 * Hello, world.
+	 * @param args - args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");
+	}
+}
